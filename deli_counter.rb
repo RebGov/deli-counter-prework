3d400e569aaaacd2ae4e55a98870740f1cbfd64f
Rebecca Govert <becci.govert@gmail.com>
2018-06-12 15:49:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli_line)
       index = 1
       katz_deli_line_current=katz_deli_line.each_with_index { |name, index| "#{index}. #{name} " }
       index += 1
-      puts "The line is currently: " + katz_deli_line_current
+      puts "The line is currently: #{katz_deli_line_current"
   end
 end
 
