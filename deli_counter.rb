05c5b00ebb0f861efc975b95f64d4916e1726fa8
Rebecca Govert <becci.govert@gmail.com>
2018-06-12 16:10:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,11 @@ katz_deli = []
 
 def line(katz_deli_line)
     if katz_deli_line.count > 1
-      output = "The line is currently:"
-      katz_deli_line.each_with_index do |name, index| 
-        output += " #{index + 1}. #{name}" 
-     end
+      output = "The line is currently:" #build output line first, then add the list items
+      katz_deli_line.each_with_index { |name, index| output += " #{index + 1}. #{name}" }
      puts output
    else
      puts "The line is currently empty."
-   end
 end
 
 def take_a_number(katz_deli_line, new_customer)
@@ -17,11 +14,11 @@ def take_a_number(katz_deli_line, new_customer)
  puts "Welcome, #{new_customer}. You are number #{katz_deli_line.length} in line." #greet and place
  end
 
-#def now_serving(katz_deli_line)
+def now_serving(katz_deli_line)
   #part 1
   #takes a person being served off the line
   # states(puts): "Currently serving name"
   #part 2 
   #if no one in line states(puts): "There is nobody waiting to be served!"
-# end
+ end
 
